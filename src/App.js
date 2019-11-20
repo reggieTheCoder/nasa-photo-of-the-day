38ff4996394b2d38c2923ce8afa10751d1fb388a
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-11-19 22:24:32 -0500

    header of page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import Logo from "./DisplayComponents/Logo";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+     <h1><Logo/>Nasa's Astronomy Photo of the Day!!!</h1>
     </div>
   );
 }
